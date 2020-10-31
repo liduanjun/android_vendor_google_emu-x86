f5c0b29c3d59d31aec387d2a619e177b2a0f3f73
Jon West <electrikjesus@gmail.com>
2020-10-31 15:32:52 -0400

    Update to 13421.73.0_atlas

--- extract-files.sh
+++ extract-files.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 # Use consistent umask for reproducible builds
 umask 022
 
-CHROMEOS_VERSION="13310.93.0_drallion"
-CHROMEOS_RECOVERY="chromeos_${CHROMEOS_VERSION}_recovery_stable-channel_mp-v2"
+CHROMEOS_VERSION="13421.73.0_atlas"
+CHROMEOS_RECOVERY="chromeos_${CHROMEOS_VERSION}_recovery_stable-channel_mp"
 
 CHROMEOS_FILENAME="$CHROMEOS_RECOVERY.bin.zip"
 CHROMEOS_URL="https://dl.google.com/dl/edgedl/chromeos/recovery/$CHROMEOS_FILENAME"
-CHROMEOS_SHA1="10ca0e6846d26a33344b5ab4b4fd5887d30edf01 $CHROMEOS_FILENAME"
+CHROMEOS_SHA1="5b3fd43c64563d1a9a331ca0808917ae1362229e $CHROMEOS_FILENAME"
 
 CHROMEOS_FILE="$PWD/$CHROMEOS_FILENAME"
 TARGET_DIR="$PWD/proprietary"
